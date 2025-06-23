664cc31f986100795d7bc0a11cefae1e92d63a71
Rafael Oliveira <rafaeloliveiraz@hotmail.com>
2025-06-23 02:50:07 +0000

    Fix grainyBG

--- index.js
+++ index.js
@@ -223,7 +223,6 @@ app.get('/:text', (req, res) => {
     const svg = `<?xml version="1.0" encoding="UTF-8"?>
     <svg width="800" height="200" xmlns="http://www.w3.org/2000/svg">
         ${bgSvg}
-        ${grainyBg}
         ${techBgSvg}
         <style>
             .banner-text-main {
